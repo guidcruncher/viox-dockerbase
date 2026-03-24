308de887fbb73c9056b96e58656ac1bcd27e0d62
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-03-24 04:09:28 +0000

    Scaffolding

--- src/scripts/playtrack.sh
+++ src/scripts/playtrack.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+stream="playtrack"
+
+if [ -z "$1" ]; then
+    echo "Format: playtrack.sh {filename or url} {stream name}"
+    echo "  Stream name is optional, default 'playtrack'"
+    echo
+    exit 1
+fi
+
+
+if [ ! -z "$2" ]; then
+    stream="$2"
+fi
+
+ffmpeg -i "$1" -f pulse -device playtrack "$stream"
