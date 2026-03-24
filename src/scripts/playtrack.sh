029ee95e524a3da96263c89c9105518044976a6d
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-03-24 12:40:45 +0000

    Scaffolding

--- src/scripts/playtrack.sh
+++ src/scripts/playtrack.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-stream="playtrack"
-
 if [ -z "$1" ]; then
-    echo "Format: playtrack.sh {filename or url} {stream name}"
-    echo "  Stream name is optional, default 'playtrack'"
+    echo "Format: playtrack.sh {filename or url}"
     echo
     exit 1
 fi
 
-
-if [ ! -z "$2" ]; then
-    stream="$2"
-fi
-
-ffmpeg -i "$1" -f pulse -device playtrack -hide_banner -loglevel error "$stream"
+ffplay -nodisp -hide_banner -loglevel error -autoexit "$1"
